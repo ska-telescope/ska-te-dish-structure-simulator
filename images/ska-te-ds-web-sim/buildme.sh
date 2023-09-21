908011b22b0d1a6cd74dbe9b6cee7844d5de8a47
Thomas Juerges <thomas.juerges@skao.int>
2023-09-21 13:03:15 +0100

    WOM-143 Remove fancy build arg because it is not needed

--- images/ska-te-ds-web-sim/buildme.sh
+++ images/ska-te-ds-web-sim/buildme.sh
@@ -1,10 +1,1 @@
-# Set the build-arg CETC_TARBALL_LOCATION to the directory within this repo
-# that contains the CETC simulator tarball. If you build the image from the top
-# level directory of this repo, then set the location to:
-# CECT_TARBALL_LOCATION=images/ska-te-ds-web-sim
-# You can then build the image like so:
-#
-# docker build --build-arg=CETC_TARBALL_LOCATION=images/ska-te-ds-web-sim -t simulator:1.0 -f images/ska-te-ds-web-sim/Dockerfile .
-#
-# If you build from the same directory where the Dockerfile is located, then just specify the current directory as it is now.
-docker build --build-arg=CETC_TARBALL_LOCATION=${PWD} -t simulator:1.0 -f Dockerfile .
+docker build -t simulator:1.0 -f Dockerfile .
