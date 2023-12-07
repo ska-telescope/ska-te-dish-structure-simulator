1ea633b55ae3e0a1c8e770be7eeb40f9cac30332
Adriaan de Beer <adebeer42@gmail.com>
2023-12-07 07:16:31 +0100

    AT-1853 new version

--- images/ska-te-ds-web-sim/runme.sh
+++ images/ska-te-ds-web-sim/runme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-
-docker run --rm -it -p 8090:8090 -p 4840:4840 -p 5005:5005 --name simulator simulator:1.0
+source ${script_dir}/version.txt
+docker run --rm -it -p 8090:8090 -p 4840:4840 -p 5005:5005 --name simulator simulator:${version}
