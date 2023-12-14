2b77270cdc9766a32ba05219b3ab66b449e1b4be
Ulrik Pedersen <ukp@observatorysciences.co.uk>
2023-12-14 14:10:14 +0000

    AT-1853: tweaks to make the container work for new sim version 1.4
    Creating the simulator dir in the contrainer.
    Set the script_dir variable in the runme.sh script.

--- images/ska-te-ds-web-sim/runme.sh
+++ images/ska-te-ds-web-sim/runme.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -euo pipefail
+script_dir=$(dirname "$0")
 source ${script_dir}/version.txt
 docker run --rm -it -p 8090:8090 -p 4840:4840 -p 5005:5005 --name simulator simulator:${version}
